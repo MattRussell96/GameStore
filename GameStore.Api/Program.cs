4bf822a4887742324bf97e3ba657e562972357af
MattRussell96 <98077789+MattRussell96@users.noreply.github.com>
2022-11-21 14:49:03 -0500

    Added Connection String

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,3 +1,6 @@
+using GameStore.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContextPool<GameStoreDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("GameStoreConnection"))
+);
 
 var app = builder.Build();
 
