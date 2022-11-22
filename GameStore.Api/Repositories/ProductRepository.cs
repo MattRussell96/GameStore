dcac008d18c30b32792fc2a6e967e7581132004c
MattRussell96 <98077789+MattRussell96@users.noreply.github.com>
2022-11-22 11:05:29 -0500

    Added product interface and methods

---
+++ GameStore.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using GameStore.Api.Entities;
+using GameStore.Api.Repositories.Contracts;
+
+namespace GameStore.Api.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task<IEnumerable<ProductCategory>> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductCategory> GetCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
