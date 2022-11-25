c06dc108c15aa0515f6901f3caac0195bbd3612d
MattRussell96 <98077789+MattRussell96@users.noreply.github.com>
2022-11-25 10:45:55 -0500

    Refactored for cleaner code and displaying by categories

---
+++ GameStore.Web/Pages/DisplayProductsBase.cs
@@ -0,0 +1,11 @@
+﻿using GameStore.Models.Dtos;
+using Microsoft.AspNetCore.Components;
+
+namespace GameStore.Web.Pages
+{
+    public class DisplayProductsBase : ComponentBase
+    {
+        [Parameter]
+        public IEnumerable<ProductDto> Products { get; set; }
+    }
+}
