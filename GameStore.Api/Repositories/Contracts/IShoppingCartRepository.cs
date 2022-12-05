8112f43b97f3eb836e91cd14d5f0ddff07f46318
MattRussell96 <98077789+MattRussell96@users.noreply.github.com>
2022-12-05 15:29:02 -0500

    Added cart interface / cart repository / and spinning loading icon

---
+++ GameStore.Api/Repositories/Contracts/IShoppingCartRepository.cs
@@ -0,0 +1,14 @@
+﻿using GameStore.Api.Entities;
+using GameStore.Models.Dtos;
+
+namespace GameStore.Api.Repositories.Contracts
+{
+    public interface IShoppingCartRepository
+    {
+        Task<CartItem> AddItem(CartItemToAddDto cartItemToAddDto);
+        Task<CartItem> UpdateQty(int id, CartItemQtyUpdateDto cartItemQtyUpdateDto);
+        Task<CartItem> DeleteItem(int id);
+        Task<CartItem> GetItem(int id);
+        Task<IEnumerable<CartItem>> GetItems(int UserId);
+    }
+}
---
+++ GameStore.Api/Repositories/ShoppingCartRepository.cs
@@ -0,0 +1,42 @@
+﻿using GameStore.Api.Data;
+using GameStore.Api.Entities;
+using GameStore.Api.Repositories.Contracts;
+using GameStore.Models.Dtos;
+
+namespace GameStore.Api.Repositories
+{
+    public class ShoppingCartRepository : IShoppingCartRepository
+    {
+        private readonly GameStoreDbContext gameStoreDbContext;
+
+        public ShoppingCartRepository(GameStoreDbContext gameStoreDbContext)
+        {
+            this.gameStoreDbContext = gameStoreDbContext;
+        }
+        
+        public Task<CartItem> AddItem(CartItemToAddDto cartItemToAddDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartItem> DeleteItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartItem> GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CartItem>> GetItems(int UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartItem> UpdateQty(int id, CartItemQtyUpdateDto cartItemQtyUpdateDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
