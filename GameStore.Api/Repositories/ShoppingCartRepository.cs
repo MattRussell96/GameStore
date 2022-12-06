9df8109293b3b1d1c1720d528b1fee4991e8789d
MattRussell96 <98077789+MattRussell96@users.noreply.github.com>
2022-12-06 09:49:36 -0500

    Implemented interface for some shopping cart methods

--- GameStore.Api/Repositories/ShoppingCartRepository.cs
+++ GameStore.Api/Repositories/ShoppingCartRepository.cs
@@ -2,6 +2,7 @@
 using GameStore.Api.Entities;
 using GameStore.Api.Repositories.Contracts;
 using GameStore.Models.Dtos;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Repositories
 {
@@ -13,25 +14,70 @@ namespace GameStore.Api.Repositories
         {
             this.gameStoreDbContext = gameStoreDbContext;
         }
-        
-        public Task<CartItem> AddItem(CartItemToAddDto cartItemToAddDto)
+
+        private async Task<bool> CartItemExists(int cartId, int productId)
         {
-            throw new NotImplementedException();
+            return await this.gameStoreDbContext.CartItems.AnyAsync(c => c.CartId == cartId && 
+                                                                     c.ProductId == productId);
         }
+        
+        public async Task<CartItem> AddItem(CartItemToAddDto cartItemToAddDto)
+        {
+            if (await CartItemExists(cartItemToAddDto.CartId, cartItemToAddDto.ProductId) == false) 
+            {
+                var item = await (from product in this.gameStoreDbContext.Products
+                                  where product.Id == cartItemToAddDto.ProductId
+                                  select new CartItem
+                                  {
+                                      CartId = cartItemToAddDto.CartId,
+                                      ProductId = product.Id,
+                                      Qty = cartItemToAddDto.Qty
+                                  }).SingleOrDefaultAsync();
+
+                if (item != null)
+                {
+                    var result = await this.gameStoreDbContext.CartItems.AddAsync(item);
+                    await this.gameStoreDbContext.SaveChangesAsync();
+                    return result.Entity;
+                }
+            }
+            return null;
+        }
+            
 
         public Task<CartItem> DeleteItem(int id)
         {
             throw new NotImplementedException();
         }
 
-        public Task<CartItem> GetItem(int id)
+        public async Task<CartItem> GetItem(int id)
         {
-            throw new NotImplementedException();
+            return await (from cart in this.gameStoreDbContext.Carts
+                          join cartItem in this.gameStoreDbContext.CartItems
+                          on cart.Id equals cartItem.CartId
+                          where cartItem.Id == id
+                          select new CartItem
+                          {
+                              Id = cartItem.Id,
+                              ProductId = cartItem.ProductId,
+                              Qty = cartItem.Qty
+                              CartId = cartItem.CartId
+                          }).SingleOrDefaultAsync();
         }
 
-        public Task<IEnumerable<CartItem>> GetItems(int UserId)
+        public async Task<IEnumerable<CartItem>> GetItems(int UserId)
         {
-            throw new NotImplementedException();
+            return await (from cart in this.gameStoreDbContext.Carts
+                          join cartItem in this.gameStoreDbContext.CartItems
+                          on cart.Id equals cartItem.CartId
+                          where cart.UserId == UserId
+                          select new CartItem
+                          {
+                              Id = cartItem.Id,
+                              ProductId = cartItem.ProductId,
+                              Qty = cartItem.Qty,
+                              CartId = cartItem.CartId
+                          }).ToListAsync();
         }
 
         public Task<CartItem> UpdateQty(int id, CartItemQtyUpdateDto cartItemQtyUpdateDto)
